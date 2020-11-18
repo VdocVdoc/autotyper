db26696a22225a2d4eebcd5aa38a1d899fc4c0fb
Gabriel <55822172+VdocVdoc@users.noreply.github.com>
2020-11-17 17:27:47 -0800

    Update autotyper.py

--- autotyper.py
+++ autotyper.py
@@ -10,28 +10,30 @@ def enter():
     keyboard.release(Key.enter)
 
 
-work = '$work'
-dep = '$dep all'
-
+to_be_typed = '-b'
+to_be_typed_2 = '$dep all'
 
 def main():
     count = 0
     try:
 
         while True:
+            """COUNTS THE AMOUNT OF TIMES THE COMMAND HAS LOOPED"""
             count += 1
+            """Types first command and then enters it. After it does the second command."""
             print('Typing...')
-            keyboard.type(work)
+            keyboard.type(to_be_typed)
             enter()
             time.sleep(0.8)
             print(f'Looped {count} times!')
-            keyboard.type(dep)
+            """Second command is not needed"""
+            keyboard.type(to_be_typed_2)
             enter()
             print('Done\n------------------')
+            """TO PREVENT WARNINGS: A 60 SECOND COOLDOWN. Can be disabled with a comment."""
             time.sleep(60)
 
     except Exception as e:
         print(e)
-
-
+       
 main()
