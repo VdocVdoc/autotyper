1014b70453db7077f880adf167a620f4dbcc3c39
Vdoc <55822172+VdocVdoc@users.noreply.github.com>
2021-04-22 07:57:21 -0700

    Update autodelete.py

--- autodelete.py
+++ autodelete.py
@@ -1,10 +1,10 @@
 from pynput.keyboard import Key, Controller as KC
 from pynput.mouse import Button, Controller as MC
 
-import time
+from time import sleep
 keyboard = KC()
 mouse = MC()
-time.sleep(5)
+sleep(5)
 
 
 def enter():
@@ -12,36 +12,56 @@ def enter():
     keyboard.release(Key.enter)
 
 
-def main():
+def delete_with_Arrows():
     try:
         # CLICK
         mouse.click(Button.left, 1)
         # UP
         keyboard.press(Key.up)
         keyboard.release(Key.up)
-        time.sleep(1.5)
+        sleep(1.5)
         with keyboard.pressed(Key.ctrl):
             keyboard.press("a")
-            time.sleep(1)
+            sleep(1)
             keyboard.press(Key.delete)
             print("delete")
             keyboard.release(Key.delete)
             keyboard.release("a")
-            time.sleep(1.5)
+            sleep(1.5)
 
         enter()
-        time.sleep(1.5)
+        sleep(1.5)
         enter()
     except Exception as e:
         print(e)
 
 
-if __name__ == '__main__':
-    x = 0
-    while True:
-        # if x == 500:
-        # break
-        main()
-        time.sleep(40)
+def delete_mouse():
+    # print(f'The current pointer position is {mouse.position}')
+    try:
+        # Click search
+        mouse.position = (1420, 126)
+        sleep(1.5)
+        mouse.click(Button.left, 1)
+        sleep(4)
+        enter()
+        # # Click first result
+        sleep(1.5)
+        mouse.position = (1264, 275)
+        sleep(2)
+        mouse.click(Button.left, 1)
+        # Click Delete
+        sleep(1.5)
+        mouse.position = (1135, 468)
+        with keyboard.pressed(Key.shift):
+            sleep(3)
+            mouse.click(Button.left, 1)
+    except Exception as e:
+        print(e)
 
-        # x = x + 1
+
+if __name__ == '__main__':
+    while True:
+        # delete_with_Arrows()
+        delete_mouse()
+        sleep(4)
